880f5b40f293f9c5c176110b1d127254114432b2
eliemoriceau <elie.moriceau@epitech.eu>
2022-11-04 12:35:54 +0100

    add toast lib

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,8 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
-import {createPinia} from "pinia";
-import {useAuth} from "@/hook/useAuth";
+import { createPinia } from "pinia";
+import { useAuth } from "@/hook/useAuth";
+import ToastPlugin from "vue-toast-notification";
+// Import one of the available themes
+// import "vue-toast-notification/dist/theme-default.css";
+import "vue-toast-notification/dist/theme-sugar.css";
 
 // Fix Property 'env' does not exist on type 'ImportMeta'.
 declare global {
@@ -14,7 +18,7 @@ declare global {
     };
   }
 }
-  
+
 loadFonts();
 
 const pinia = createPinia();
@@ -23,6 +27,7 @@ createApp(App)
   .use(pinia)
   .use(router)
   .use(vuetify)
+  .use(ToastPlugin, { position: "bottom-right" })
   .use((app, options) => {
     const auth = useAuth();
     auth.loginFromStorage();
