126d5cac9de23fd0e3dc4f49352dd1fb8541a13a
nico <nicolas.sansd@gmail.com>
2022-11-07 11:14:56 +0100

    added delete team modal and edit team modal

--- frontend/src/views/teams/TeamListPage.vue
+++ frontend/src/views/teams/TeamListPage.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import CreateTeamModal from "@/components/modals/CreateTeamModal.vue";
+import CreateTeamModal from "@/components/modals/teams/CreateTeamModal.vue";
 import {useRouter} from "vue-router";
 import { ref, onMounted } from 'vue';
 import {useAuthStore} from "@/store/AuthStore";
--- frontend/src/views/teams/TeamPage.vue
+++ frontend/src/views/teams/TeamPage.vue
@@ -1,22 +1,34 @@
 <script lang="ts" setup>
 
-import {useRoute} from "vue-router";
+import {useRoute, useRouter} from "vue-router";
 import {getTeam} from "@/services/team";
 import {useAuthStore} from "@/store/AuthStore";
 import {onMounted, ref} from "vue";
 import {ITeam} from "@/dto/team";
 import TeamMembersList from "@/components/teams/TeamMembersList.vue";
+import EditTeamModal from "@/components/modals/teams/EditTeamModal.vue";
+import DeleteTeamModal from "@/components/modals/teams/DeleteTeamModal.vue";
+import {useToast} from "vue-toast-notification";
 const route = useRoute();
 const auth = useAuthStore();
 
+enum ModalType {
+  Edit,
+  AddUser,
+  DeleteTeam,
+}
+
+const router = useRouter();
+const toast = useToast();
+
 const team = ref<undefined | ITeam>(undefined);
+const open = ref<undefined | ModalType>(undefined);
 
 const loadTeam = (teamId: string) => {
-  console.log("load team");
   getTeam(auth.accessToken, teamId, (teams, error) => {
-    console.log(teams, error);
     if (error) {
       console.log(error);
+      toast.error("Une erreur est survenue ! #1300");
     } else if (teams !== undefined) {
       team.value = teams;
     }
@@ -24,25 +36,42 @@ const loadTeam = (teamId: string) => {
 }
 
 onMounted(() => {
-  console.log("mounted !");
   loadTeam(route.params["id"] as any);
 });
+
+const onDismissModal = () => {
+  open.value = undefined;
+}
+
+const onTeamEdited = (newTeam: ITeam) => {
+  loadTeam(team.value["id"]);
+  toast.success("Votre team à bien été modifié");
+  open.value = undefined;
+}
+
+const onTeamDeleted = (newTeam: ITeam) => {
+  toast.success("Votre team à bien été supprimé");
+  open.value = undefined;
+  router.push({name: 'teams'})
+}
 
 </script>
 
 <template>
   <div v-if="team !== undefined">
+    <EditTeamModal :open="open === ModalType.Edit" :on-dismiss="onDismissModal" :on-success="onTeamEdited" :team="team"/>
+    <DeleteTeamModal :open="open === ModalType.DeleteTeam" :on-dismiss="onDismissModal" :on-success="onTeamDeleted" :team="team"/>
     <h1>{{team.name}}</h1>
     <h4>Mise à jour le {{new Date(team.updated_at).toLocaleString()}}</h4>
     <div class="actions">
       <v-btn
-          @click="onClickEdit"
+          @click="open = ModalType.Edit"
           color="info"
           rounded
           variant="elevated"
-          text>Editer la team</v-btn>
+          text>Modifier la team</v-btn>
       <v-btn
-          @click="onClickDelete"
+          @click="open = ModalType.DeleteTeam"
           color="error"
           rounded
           variant="elevated"
