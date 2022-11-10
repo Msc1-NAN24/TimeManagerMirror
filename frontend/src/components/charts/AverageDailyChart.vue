0850a3e133688b94d541fc4cd1312f2a78e3bc69
eliemoriceau <62339484+eliemoriceau@users.noreply.github.com>
2022-11-10 17:12:06 +0100

    feat: average dailyChart (#76)

--- frontend/src/components/charts/AverageDailyChart.vue
+++ frontend/src/components/charts/AverageDailyChart.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Line } from 'vue-chartjs'
+import { Line } from "vue-chartjs";
 
 import {
   Chart as ChartJS,
@@ -9,81 +9,140 @@ import {
   BarElement,
   CategoryScale,
   LinearScale,
-  LineElement, PointElement
-} from 'chart.js'
-import {ref, watch} from "vue";
+  LineElement,
+  PointElement,
+} from "chart.js";
+import { ref, watch } from "vue";
 import User from "@/components/User.vue";
-import {IWorkingTime} from "@/dto/workingTime";
-import {DateTime} from "luxon";
+import { IWorkingTime } from "@/dto/workingTime";
+import { DateTime } from "luxon";
 
 export type UserWorkingTime = {
-  user: User,
+  user: User;
   times: IWorkingTime[];
-}
+};
 
-const colors = ["#2ecc71", "#3498db", "#9b59b6", "#e74c3c", "#e67e22", "#f1c40f", "#34495e"]
+const colors = [
+  "#0aecf3",
+  "#3498db",
+  "#9b59b6",
+  "#e74c3c",
+  "#e67e22",
+  "#f1c40f",
+  "#34495e",
+];
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, LineElement, PointElement)
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  LineElement,
+  PointElement
+);
 
-const props = defineProps(['times', 'numberOfMonths', 'onMonthChange', 'reload']);
+const props = defineProps([
+  "times",
+  "numberOfMonths",
+  "onMonthChange",
+  "reload",
+  "average",
+]);
 const now = DateTime.now();
-const date = ref({month: now.month - 1 , year: now.year});
+const date = ref({ month: now.month - 1, year: now.year });
 
 const chartOptions = {
   responsive: true,
   maintainAspectRatio: false,
-}
+};
 
 const chartData = ref({
-  labels: [...Array.from({length: props.numberOfMonths},(v,k)=> `${k+1}`)],
+  labels: [
+    ...Array.from({ length: props.numberOfMonths }, (v, k) => `${k + 1}`),
+  ],
   tension: 0.3,
   datasets: [
     {
-      label: 'Nicolas',
+      label: "Nicolas",
       data: [40, 39, 10, 40, 39, 80, 40],
       tension: 0.2,
       borderColor: "#c45850",
-      fill: false
+      fill: false,
     },
     {
-      label: 'Hugo',
+      label: "Hugo",
       data: [20, 10, 30, 35, 39, 50, 60],
       tension: 0.2,
       borderColor: "#5079c4",
-      fill: false
-    }
-  ]
+      fill: false,
+    },
+  ],
 });
 
-watch(() => props.times, (times: UserWorkingTime[]) => {
-  const data = times.map((t, index) => {
-    let connectedInSection = {};
-    if (t.times.length > 0) {
-      for (let i = 0; i < props.numberOfMonths; i++) {
-        connectedInSection[i] = t.times.filter((wt) => DateTime.fromISO(wt.start).day === i).reduce((last, time) => last + DateTime.fromISO(time.end).diff(DateTime.fromISO(time.start), 'hours').toObject().hours, 0);
+watch(
+  () => props.times,
+  (times: UserWorkingTime[]) => {
+    const data = times.map((t, index) => {
+      let connectedInSection = {};
+      if (t.times.length > 0) {
+        for (let i = 0; i < props.numberOfMonths; i++) {
+          connectedInSection[i] = t.times
+            .filter((wt) => DateTime.fromISO(wt.start).day === i)
+            .reduce(
+              (last, time) =>
+                last +
+                DateTime.fromISO(time.end)
+                  .diff(DateTime.fromISO(time.start), "hours")
+                  .toObject().hours,
+              0
+            );
+        }
       }
-    }
-    return {
-      label: `${t.user.firstname} ${t.user.lastname.toUpperCase()}`,
+      return {
+        label: `${t.user.firstname} ${t.user.lastname.toUpperCase()}`,
+        tension: 0.2,
+        borderColor: colors[index % colors.length],
+        fill: false,
+        data: [
+          ...Array.from({ length: props.numberOfMonths }, (v, k) => k + 1).map(
+            (day) => {
+              const section = Object.keys(connectedInSection).find(
+                (a) => a == day
+              );
+              if (section == undefined) return 0;
+              return connectedInSection[section];
+            }
+          ),
+        ],
+      };
+    });
+
+    let averageDatasets = {
+      label: "Moyenne",
       tension: 0.2,
-      borderColor: colors[index % colors.length],
-      fill: false,
-      data: [...Array.from({length: props.numberOfMonths},(v,k)=> k+1).map((day) => {
-        const section = Object.keys(connectedInSection).find((a) => a == day);
-        if (section == undefined)
-          return 0;
-        return connectedInSection[section];
-      })],
-    }
-  });
-  chartData.value = {
-    labels: [...Array.from({length: props.numberOfMonths},(v,k)=> `${k+1}`)],
-    tension: 0.3,
-    datasets: [
-        ...data
-    ]
+      borderColor: "#00ff58",
+      borderWidth: 5,
+      data: [
+        ...Array.from({ length: props.numberOfMonths }, (v, k) => k + 1).map(
+          (day) =>
+            data
+              .map((user) => user.data[day - 1])
+              .reduce((acc, currentValue) => acc + currentValue) / times.length
+        ),
+      ],
+    };
+
+    chartData.value = {
+      labels: [
+        ...Array.from({ length: props.numberOfMonths }, (v, k) => `${k + 1}`),
+      ],
+      tension: 0.3,
+      datasets: props.average ? [...data, averageDatasets] : [...data],
+    };
   }
-});
+);
 
 </script>
 
@@ -100,12 +159,14 @@ watch(() => props.times, (times: UserWorkingTime[]) => {
             icon="mdi-refresh"
         ></v-btn>
       </div>
-      <Datepicker :clearable="false" class="picker" v-model="date" month-picker @update:modelValue="props.onMonthChange"/>
+      <Datepicker
+        :clearable="false" class="picker"
+        v-model="date"
+        month-picker
+        @update:modelValue="props.onMonthChange"
+      />
     </div>
-    <Line
-        :chart-options="chartOptions"
-        :chart-data="chartData"
-    />
+    <Line :chart-options="chartOptions" :chart-data="chartData" />
   </div>
 </template>
 <style scoped>
@@ -116,13 +177,13 @@ watch(() => props.times, (times: UserWorkingTime[]) => {
 }
 
 .picker {
-  width: 350px
+  width: 350px;
 }
 </style>
 
 <style scoped>
-  .root {
-    margin-top: 20px;
-    margin-bottom: 20px;
-  }
+.root {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
 </style>
